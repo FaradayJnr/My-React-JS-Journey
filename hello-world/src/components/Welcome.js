8b694f090e6d1a6e86fc5b12b80e5937c24cf1b8
FaradayJnr <faradafoundationgh@gmail.com>
2023-03-24 03:21:04 -0700

    initial commit

---
+++ hello-world/src/components/Welcome.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+//state lass component
+class Welcome extends Component{
+    render(){
+        return <h1>You warmly are welcome</h1>
+    }
+}
+export default Welcome
