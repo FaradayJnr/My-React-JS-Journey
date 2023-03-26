e1e43a4e7088715d524a27c11b9df2267e560058
FaradayJnr <faradafoundationgh@gmail.com>
2023-03-25 21:00:39 -0700

    setState

--- hello-world/src/components/Greet.js
+++ hello-world/src/components/Greet.js
@@ -3,7 +3,11 @@ import React from 'react'
 // function Greet(){
 //     return <h1>Hello Emma</h1>
 // }
+
+//jsx format = functional component
+//prop - to pass argument to functions
 const Greet = props => {
+//log the console value (prop)
 console.log(props)
 return (
 <div>
