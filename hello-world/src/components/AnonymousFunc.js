28d1ae52648ad397a648f7993622c56411ec6b24
FaradayJnr <faradafoundationgh@gmail.com>
2023-03-26 20:02:16 -0700

    conditional

--- hello-world/src/components/AnonymousFunc.js
+++ hello-world/src/components/AnonymousFunc.js
@@ -1,12 +1,16 @@
 import React from 'react'
-
+let age = 10
 //anonymous function
-function AnonymousFunc() {
-  return (
-    <div>
-      <button onClick={() => {console.log('I am anonymous');}}>Tap on it</button>
-    </div>
-  )
-}
+// function AnonymousFunc() {
+//   return (
+//     <div className="anonymous">
+//       <button onClick={() => {console.log('I am anonymous');}}>Tap on it</button>
+//     </div>
+//   )
+// }
 
+//conditional (if...else) --> Tenary operators
+const AnonymousFunc = () =>{
+    return age > 10 ? <div>Hello</div>: <div>Hi</div>
+} 
 export default AnonymousFunc
